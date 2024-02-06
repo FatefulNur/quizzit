a2568cc798f4e2a4df62d766b4ab391dbb9303ce
Sahin Uddin Rony <sahincseiu@gmail.com>
2024-02-06 18:46:08 +0600

    Modify Model Classes

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Enums\QuizType\QuizType;
+use App\Enums\QuizType;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Enums\UserType\UserType;
+use App\Enums\UserType;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
