0d6681233a7072db5c2a2651ff2a7bd885edbc7a
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-06 19:52:46 +0600

    Move ProfileTest into User folder

--- tests/Feature/User/ProfileTest.php
+++ tests/Feature/User/ProfileTest.php
@@ -8,7 +8,7 @@ test('profile page is displayed', function () {
 
     $this->actingAs($user);
 
-    $response = $this->get('/profile');
+    $response = $this->get(route('user.profile'));
 
     $response
         ->assertOk()
