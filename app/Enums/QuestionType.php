e25cafb26f44c33f9752f29bc2e9617694683158
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-06 19:21:30 +0600

    Add getLabel method in QuestionType enum

--- app/Enums/QuestionType.php
+++ app/Enums/QuestionType.php
@@ -8,4 +8,14 @@ enum QuestionType: string
     case LONG_TEXT = 'long_text';
     case RADIO = 'radio';
     case CHECKBOX = 'checkbox';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::SHORT_TEXT => 'Short Text',
+            self::LONG_TEXT => 'Long Text',
+            self::RADIO => 'Radio Button',
+            self::CHECKBOX => 'Check Lists',
+        };
+    }
 }
