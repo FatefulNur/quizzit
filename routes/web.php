3005dd9350cf492710875bbfb0969fa761bd4fd0
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-06 19:24:57 +0600

    Modify web.php file routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\User\ProfileController;
+use App\Http\Controllers\User\QuizController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', HomeController::class);
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\User\DashboardController;
 
 require __DIR__ . '/auth.php';
+
+Route::get('/', HomeController::class)->name('home');
+
+Route::middleware(['auth'])->prefix('user')->name('user.')->group(function () {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+    Route::get('/profile', ProfileController::class)->name('profile');
+    Route::resource('quizzes', QuizController::class)->except('index');
+});
