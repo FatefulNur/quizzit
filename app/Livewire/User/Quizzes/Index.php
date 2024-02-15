efd655145997043421a3555cd840b467aad8e1e2
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-15 17:18:54 +0600

    Add Quiz Index livewire component

--- app/Livewire/User/Quizzes/Index.php
+++ app/Livewire/User/Quizzes/Index.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire\User\Quizzes;
 
+use App\Models\Quiz;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.user.quizzes.index');
+        return view('livewire.user.quizzes.index', [
+            'quizzes' => Quiz::select([
+                'title',
+                'description',
+                'type',
+                'marks_total',
+                'created_at',
+            ])->where('user_id', auth()->id())->simplePaginate(10),
+        ]);
     }
 }
