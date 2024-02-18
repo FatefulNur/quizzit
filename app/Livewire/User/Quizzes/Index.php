ab8efd162b15e0a0e0cdbd38f7fa11b090872831
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-18 15:47:18 +0600

    Put Quiz query parameters into history stack

--- app/Livewire/User/Quizzes/Index.php
+++ app/Livewire/User/Quizzes/Index.php
@@ -11,10 +11,10 @@ class Index extends Component
 {
     use WithPagination;
 
-    #[Url(except: '')]
+    #[Url(except: '', history: true)]
     public string $date = '';
 
-    #[Url(except: false)]
+    #[Url(except: false, history: true)]
     public $isPrivate = false;
 
     public function queryParams()
