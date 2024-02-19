3ff70c551343d1722307e93a3905a1395abf95bd
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-19 08:33:03 +0600

    Set nullable user_id foreign for user_responses table

--- database/migrations/2024_02_04_101505_create_user_responses_table.php
+++ database/migrations/2024_02_04_101505_create_user_responses_table.php
@@ -17,6 +17,7 @@ return new class extends Migration {
             $table->integer('result');
             $table
                 ->foreignIdFor(User::class)
+                ->nullable()
                 ->constrained()
                 ->cascadeOnDelete();
             $table
