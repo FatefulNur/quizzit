b1c71cc5b125aea846bd9516ad0d65985858e366
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-21 17:43:51 +0600

    Remove unwanted imports in seeder files

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Enums\Role;
-use App\Enums\UserType;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
