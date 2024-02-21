a59506774cf1dec5a7bf0d759e5f5758b52790f0
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-21 08:31:15 +0600

    Add user response Index component skeleton

---
+++ app/Livewire/User/Responses/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User\Responses;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.responses.index');
+    }
+}
