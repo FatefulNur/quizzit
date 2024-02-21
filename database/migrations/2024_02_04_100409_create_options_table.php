ba37f6804673da0cc90d5f86a68ebf1b97bf50c6
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-21 08:32:48 +0600

    Set nullable options label column

--- database/migrations/2024_02_04_100409_create_options_table.php
+++ database/migrations/2024_02_04_100409_create_options_table.php
@@ -13,7 +13,8 @@ return new class extends Migration {
     {
         Schema::create('options', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('label');
+            $table->string('label')
+                ->nullable();
             $table->boolean('is_correct');
             $table
                 ->foreignIdFor(Question::class)
