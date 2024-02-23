6d2afed45ea0a0c9fb58a452a107915fd9a513e8
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-23 14:56:28 +0600

    Add answers relation into UserResponse model

--- app/Models/UserResponse.php
+++ app/Models/UserResponse.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UserResponse extends Model
 {
@@ -19,4 +20,9 @@ class UserResponse extends Model
     protected $casts = [
         'result' => 'integer',
     ];
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
