d18ed8a78b3c12d1acc162a4c12f3b0a255e605f
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-23 14:57:07 +0600

    Add service class for user response and answer generation

---
+++ app/Services/UserResponseService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Quiz;
+use App\Models\Question;
+use App\Models\UserResponse;
+use Illuminate\Support\Facades\DB;
+
+class UserResponseService
+{
+    public static function store(Quiz $quiz, array $data)
+    {
+        return DB::transaction(function () use ($quiz, $data) {
+            $result = 0;
+            $userResponse = UserResponse::create([
+                'result' => $result,
+                'user_id' => auth()?->id(),
+                'quiz_id' => $quiz->id,
+            ]);
+
+            $formattedAnswers = [];
+
+            foreach ($data as $answers) {
+                $question = Question::select(['id', 'marks', 'type'])->find($answers['question_id']);
+
+                $correctOptionsLabel = $question->options()
+                    ->select('label')
+                    ->where('is_correct', true)
+                    ->get()->map->label->toArray();
+
+                if ($question->isShortText() || $question->isLongText()) {
+                    if (count($correctOptionsLabel)) {
+                        $correctOptionsLabel = explode(',', $correctOptionsLabel[0]);
+                    }
+                }
+
+                $correct = implode(', ', $correctOptionsLabel);
+                $isMarked = false;
+
+                foreach ($answers['answer'] as $key => $answer) {
+
+                    if (!$isMarked && in_array($answer, $correctOptionsLabel)) {
+                        $result += $question->marks;
+                        $isMarked = true;
+                    }
+
+                    $formattedAnswers[] = [
+                        'correct' => $correct ?? '',
+                        'answer' => $answer,
+                        'option_id' => $answers['option_id'][$key],
+                        'question_id' => $answers['question_id'],
+                    ];
+                }
+            }
+
+            $userResponse->answers()->createMany($formattedAnswers);
+
+            $userResponse->result = $result;
+            $userResponse->save();
+
+            return $userResponse;
+        });
+    }
+}
