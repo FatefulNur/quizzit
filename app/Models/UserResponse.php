57861f3fc3dc7ab15758b16f21ee237e8a543b3e
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-23 19:23:55 +0600

    Add belongsTo relation method into UserResponse model

--- app/Models/UserResponse.php
+++ app/Models/UserResponse.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UserResponse extends Model
@@ -24,5 +25,15 @@ class UserResponse extends Model
     public function answers(): HasMany
     {
         return $this->hasMany(Answer::class);
+    }
+
+    public function user(): ?BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function quiz(): BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
     }
 }
