582fafde52f798007564feb305f68488e86ae779
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-27 07:58:02 +0600

    Add billings Index component for User

---
+++ app/Livewire/User/Billings/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User\Billings;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.billings.index');
+    }
+}
