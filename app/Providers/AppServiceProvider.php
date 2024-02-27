16c4085737980c2b29e3fbf3645f20c7ef827857
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-27 08:00:32 +0600

    Add command to generate plans

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Http::macro('lemonsqueezy', function () {
+            return Http::retry(3, 100)->withToken(config('lemonsqueezy.secret'))->withHeaders([
+                'Accept' => 'application/vnd.api+json',
+                'Content-Type' => 'application/vnd.api+json',
+            ])->baseUrl('https://api.lemonsqueezy.com');
+        });
     }
 }
