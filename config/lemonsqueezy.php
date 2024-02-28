50e01cc1edbb3867579a753d047c3d474d0d5558
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-28 21:49:51 +0600

    Add webhook configuration of lemonsqueezy

--- config/lemonsqueezy.php
+++ config/lemonsqueezy.php
@@ -3,4 +3,12 @@
 return [
     // api key
     "secret" => env('LEMONSQUEEZY_SECRET'),
+
+    "webhook" => [
+        "subscription" => [
+            "created" => [
+                "sign" => env('LEMONSQUEEZY_WEBHOOK_SUBSCRIPTION_CREATED_SIGN'),
+            ],
+        ],
+    ],
 ];
