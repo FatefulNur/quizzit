273b613a7a0ab1e7de871e5d6aacab4e35c3c033
FatefulNur <km.nurunnabi66@gmail.com>
2024-02-29 22:30:56 +0600

    Add filament styles into app component layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,8 +13,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @filamentStyles
+    @vite('resources/css/app.css')
 </head>
 
 <body class="font-sans antialiased">
@@ -35,6 +35,9 @@
             {{ $slot }}
         </main>
     </div>
+
+    @filamentScripts
+    @vite('resources/js/app.js')
 </body>
 
 </html>
