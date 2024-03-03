64b1b88b575981aa11ddc25cf251b3b6819a78ac
FatefulNur <km.nurunnabi66@gmail.com>
2024-03-03 16:35:51 +0600

    Add @filamentStyles directive into all layouts

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -14,13 +14,13 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+
+    @filamentStyles
     <style>
         html {
             scroll-behavior: smooth;
         }
     </style>
-
-    <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
