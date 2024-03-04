2911803385d719e8ff542e1d0ac7bb91f830c3a8
FatefulNur <km.nurunnabi66@gmail.com>
2024-03-04 20:53:15 +0600

    Modify no-content component

--- resources/views/components/no-content.blade.php
+++ resources/views/components/no-content.blade.php
@@ -1,12 +1,13 @@
 <div
     class="shadow-md rounded-sm p-3 flex text-center mx-auto max-w-lg w-full bg-white border-none min-h-40 *:bg-gray-100">
-    <div class="flex flex-col items-center gap-3 justify-center flex-1 p-4 font-bold text-gray-500 text-lg">
+    <div class="flex flex-col items-center justify-center flex-1 gap-3 p-4 text-lg font-bold text-gray-500">
         {{ $slot->isEmpty() ? 'No Content' : $slot }}
 
         <div class="space-x-2">
-            <a href="{{ url()->previous() }}" class="text-blue-600 p-2 text-base font-extrabold">&LeftArrow; Get
-                Back</a> or
-            <a href="{{ route('user.dashboard') }}" class="text-blue-600 p-2 text-base font-extrabold">Visit
+            <a href="{{ url()->previous() }}" class="p-2 text-base font-extrabold text-blue-600">&LeftArrow; Get
+                Back</a>
+            |
+            <a href="{{ route('user.dashboard') }}" class="p-2 text-base font-extrabold text-blue-600">Visit
                 Dashboard</a>
         </div>
     </div>
