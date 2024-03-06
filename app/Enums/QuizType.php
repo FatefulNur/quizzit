215f77a584bf3a7032d5494aaca35ab82a53b990
FatefulNur <km.nurunnabi66@gmail.com>
2024-03-06 22:07:35 +0600

    Add label, color and icon contract in QuizType enum

--- app/Enums/QuizType.php
+++ app/Enums/QuizType.php
@@ -2,8 +2,36 @@
 
 namespace App\Enums;
 
-enum QuizType: string
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
+
+enum QuizType: string implements HasColor, HasLabel, HasIcon
 {
     case PRIVATE = 'private';
     case PUBLIC = 'public';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::PRIVATE => 'Private',
+            self::PUBLIC => 'Public',
+        };
+    }
+
+    public function getColor(): string
+    {
+        return match ($this) {
+            self::PRIVATE => 'danger',
+            self::PUBLIC => 'success',
+        };
+    }
+
+    public function getIcon(): string
+    {
+        return match ($this) {
+            self::PRIVATE => 'heroicon-m-x-mark',
+            self::PUBLIC => 'heroicon-m-check',
+        };
+    }
 }
